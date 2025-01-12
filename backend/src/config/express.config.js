5e70289825fc226b42ff8f61a33f9a883779328b
Ad-pkh <adarsha.pokharel@outlook.com>
2025-01-12 09:09:00 +0545

    initial setup

---
+++ backend/src/config/express.config.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const router = require("./router.config");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+app.use((req, res, next) => {
+  next({ statusCode: 401, message: "route not defined " });
+});
+
+// to do error handling
+
+module.exports = app;
