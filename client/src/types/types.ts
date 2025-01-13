842b07e6c4deadacd1e5d3df629f848753a42822
Devendra <devendrakhatri904@gmail.com>
2025-01-13 10:31:17 +0545

    completed user context

---
+++ client/src/types/types.ts
@@ -0,0 +1,8 @@
+export interface UserType {
+  _id: string;
+  name: string;
+  email: string;
+  address: string;
+  role: string; // Example: 'organization', 'admin', etc.
+  status: string; // Example: 'active', 'inactive', etc.
+}
