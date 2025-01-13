eb95acee8fdecb3b880f8a56bc694173803b538d
Devendra <devendrakhatri904@gmail.com>
2025-01-13 08:04:52 +0545

    fixed import in home page

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Header from "../Header";
+import UserDashboard from "./UserDashboard";
+import OrgDashboard from "./OrgDashboard";
+import { useState } from "react";
 
 const Home = () => {
   const [userType, setUserType] = useState(true);
